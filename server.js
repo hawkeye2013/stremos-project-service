32212c000a79515fbf69fe4c5b414ce05d1bb9f3
Tucker Hawkinson <7201606-tucker.hawkinson@users.noreply.gitlab.com>
2020-12-30 20:08:22 -0500

    started organizing server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+// NODE IMPORTS
+
+// EXPRESS IMPORTS
 const express = require('express');
 const app = express();
-const port = 4000;
+
+// SERVER CONFIG
+const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
